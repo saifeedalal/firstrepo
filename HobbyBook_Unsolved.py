1d5b024bd46f0df8ad6594841e14ea56d40a6cf9
Saifee D <saifee.dalal@gmail.com>
2018-07-10 20:09:10 -0500

    Sample program

---
+++ HobbyBook_Unsolved.py
@@ -0,0 +1,13 @@
+# @TODO: Your code here
+
+myDictionary = {"name":"Saifee",
+                "age":"34",
+                "Hobbies":["Movies","Soccer","Coding"],
+                "Wakeuptime":{
+                                "Monday":"6:30",
+                                "Tuesday":"6:45",
+                                "Wednesday":"6:30",
+                                "Thursday":"7:00",
+                                "Friday":"7:30"}}
+
+print(f'{myDictionary["name"]} has {myDictionary["Hobbies"][1]} as a hobby and he wakes up at {myDictionary["Wakeuptime"]["Friday"]} on Friday')
